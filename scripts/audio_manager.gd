54e82f5d4f0259dfb4e19f3afb300240beefe61e
rrouille <rrouille@rrouille.local>
2024-04-20 12:43:05 +0200

    simple ui design and sound play

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var music_player: AudioStreamPlayer = $MusicPlayer
+@onready var sound_player: AudioStreamPlayer = $SoundPlayer
+
+func _ready():
+	pass # Replace with function body.
+
+func play_button_sound():
+	sound_player.play()
+
+func play_music_sound():
+	music_player.play()
+
+
+func _on_music_player_finished():
+	music_player.play()
